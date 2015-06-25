a112b63477edffa2276208652aa6541366a36d77
Pavel Gorlov <i@pavliko.com>
2015-06-25 23:34:24 +0700

    fix(example): New Atom 1.0 linter :+1:

--- example/script.js
+++ example/script.js
@@ -4,6 +4,6 @@ var open = require("open");
 var fs = require('fs');
 var css = fs.readFileSync('./example/style.css').toString();
 
-css = postcss([ postcssSVG({paths: ['./example']}) ]).process(css).css
-fs.writeFileSync('./example/output.css', css)
+css = postcss([ postcssSVG({paths: ['./example']}) ]).process(css).css;
+fs.writeFileSync('./example/output.css', css);
 open("./example/index.html");
